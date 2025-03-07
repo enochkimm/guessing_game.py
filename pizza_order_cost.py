f60d7dd59ba11955032abad44909518ddf14a820
Enoch Kim <enochk0919@gmail.com>
2025-03-07 09:50:43 -0500

    Fixed order and added more features

--- pizza_order_cost.py
+++ pizza_order_cost.py
@@ -3,18 +3,29 @@ PRINT "Welcome to Enoch's Pizza!"
 #input pizza size
 PRINT "Enter pizza size (small or large): "
 INPUT pizza_size
-
-PRINT "Enter number of additional toppings: "
-INPUT additional_topics_num
-
-PRINT "Enter delivery distance (miles): "
-INPUT delivery_distance_miles
+SET pizza_size = LOWERCASE(pizza_size)
+WHILE pizza_size != "small" and "pizza_size" != "large"
 
 #determine pizza base price and seeing if size is valid
 IF pizza_size == "small" THEN
     SET pizza_price = 8
-ELIF pizza_slice == "large" THEN
+ELSE pizza_slice == "large" THEN
     SET pizza_price = 12
-ELSE
-    PRINT "Please enter a valid pizza size."
+ENDIF
+
+#get # of toppings
+PRINT "Enter number of additional toppings: "
+INPUT additional_toppings_num
+SET toppings_cost = additional_toppings_num
+
+#delivert distance
+PRINT "Enter delivery distance (miles): "
+INPUT delivery_distance_miles
+IF delivery_distance <= 5 THEN
+    SET delivery_cost = 2
+ELSE 
+    SET delivery cost = 2 + (delivery_distance - 5)
+ENDIF
+
+
 
