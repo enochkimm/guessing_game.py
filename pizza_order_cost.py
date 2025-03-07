1c555e14ceb035b58e7b3200a54b7c1b01afd384
Enoch Kim <enochk0919@gmail.com>
2025-03-07 09:41:22 -0500

    Fix order of functions

--- pizza_order_cost.py
+++ pizza_order_cost.py
@@ -7,3 +7,14 @@ INPUT pizza_size
 PRINT "Enter number of additional toppings: "
 INPUT additional_topics_num
 
+PRINT "Enter delivery distance (miles): "
+INPUT delivery_distance_miles
+
+#determine pizza base price and seeing if size is valid
+IF pizza_size == "small" THEN
+    SET pizza_price = 8
+ELIF pizza_slice == "large" THEN
+    SET pizza_price = 12
+ELSE
+    PRINT "Please enter a valid pizza size."
+
