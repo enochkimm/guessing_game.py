16419e23b18d92283d28d123a319f3a85f44d6fd
Enoch Kim <enochk0919@gmail.com>
2025-03-07 09:34:34 -0500

    wrote psuedocode for pizza_size and toppings input

---
+++ pizza_order_cost.py
@@ -0,0 +1,9 @@
+PRINT "Welcome to Enoch's Pizza!"
+
+#input pizza size
+PRINT "Enter pizza size (small or large): "
+INPUT pizza_size
+
+PRINT "Enter number of additional toppings: "
+INPUT additional_topics_num
+
